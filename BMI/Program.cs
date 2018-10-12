b56494fb63b1d2d9b187df48262bf8dc4a8f2401
Abicharan <37925180+thenosewizard@users.noreply.github.com>
2018-10-12 18:45:07 +0800

    Add files via upload

---
+++ BMI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your weight in kilograms?");
+            float kg = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your height in meters?");
+            float height = float.Parse(Console.ReadLine());
+
+            float bmi = kg / (height * height);
+            Console.WriteLine("Your BMI is : {0}", bmi);
+        }
+    }
+}
